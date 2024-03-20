01a62e2f2ffbb99233515f02df56bac298f89b67
CaihaoWeng <164138933+CaihaoWeng@users.noreply.github.com>
2024-03-20 16:15:16 +0900

    Add files via upload

---
+++ Wch/src/demo.java
@@ -0,0 +1,13 @@
+
+public class demo {
+	public static void main(String[] args) {
+	System.out.println("Hello Word");
+	for(int j = 0; j<10; j++) {
+		System.out.println("Hello Word" + j);
+		if( j>5) {
+			System.out.println("结束啦" + j);
+			break;
+		}
+	}
+	}
+}
